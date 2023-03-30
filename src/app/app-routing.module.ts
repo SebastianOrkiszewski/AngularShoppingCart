7aa9c01f86023c3392b1c09c0eb18f3c56c312f5
SebastianOrkiszewski <s.orkiszewski@gmail.com>
2023-03-30 12:15:42 +0200

    Add login to routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ArticlesComponent } from './component/articles/articles.component';
+import { LoginComponent } from './component/login/login.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'articles', pathMatch: 'full'},
-  {path: 'articles', component: ArticlesComponent}
+  {path: 'articles', component: ArticlesComponent},
+  {path: 'login', component: LoginComponent}
 ];
 
 @NgModule({
